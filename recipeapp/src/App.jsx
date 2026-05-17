ffdf3ece18318fc9ddf61d507c117f34bdbbc9e4
Karishma <karishma.7022@gmail.com>
2026-05-17 11:56:24 +0530

    set up app.jsx with initial state

--- recipeapp/src/App.jsx
+++ recipeapp/src/App.jsx
@@ -1,8 +1,15 @@
 import {useState} from "react";
+import initialRecipes from "./data";
 
 
 function App(){
-  const [recipes,setRecipes] = useState([]);
+  const [recipes,setRecipes] = useState(initialRecipes);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  return(
+    <div>
+      Recipe App 
+    </div>
+  )
 }
 
 export default App;
